6d3e1c1a11ccd83dd8d1d7e0ba45f30df960773b
Aluno <Aluno@DESKTOP-O31M9M7>
2025-03-13 08:19:30 -0300

    tudo pronto

--- Daniel-SalarioLiquido/Form1.Designer.cs
+++ Daniel-SalarioLiquido/Form1.Designer.cs
@@ -111,10 +111,10 @@
             // 
             // label4
             // 
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(260, 9);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(89, 9);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(489, 45);
+            this.label4.Size = new System.Drawing.Size(831, 62);
             this.label4.TabIndex = 11;
             this.label4.Text = "Calculadora de Salário Líquido";
             // 
